0b792a7f1ce8e7a638f212c4264f627ffa6c723e
Sly Flores <sly@christian.com.ph>
2021-04-25 23:11:02 +0800

    Questionnaires model

---
+++ app/Models/Questionnaire.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Questionnaire extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'enrollment_id',
+        'answers',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'updated_at',
+    ];
+    
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+
+    ];
+
+    /**
+     * @param $value
+     * @return false|string
+     */
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->format('F j, Y h:i A');
+    }
+
+    public function setAnswersAttribute($value)
+    {
+        $this->attributes['answers'] = serialize($value);
+    }
+    
+    public function getAnwersAttribute($value)
+    {
+        return unserialize($value);
+    }
+}
