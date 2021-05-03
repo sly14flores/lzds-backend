b9cf2a6c886afd0f99646bb0c6100c77dedc5cc8
Sly Flores <sly@christian.com.ph>
2021-05-03 12:14:55 +0800

    Update EnrollmentNotification.php

--- app/Notifications/EnrollmentNotification.php
+++ app/Notifications/EnrollmentNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class EnrollmentNotification extends Notification
+class EnrollmentNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
