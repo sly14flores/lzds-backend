b273b53bc4637200f14cf312fd30651b72fe6d22
Sly Flores <sly@christian.com.ph>
2021-08-29 23:43:55 +0800

    ...

---
+++ app/Console/Commands/StudentsCredentials.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\Student;
+use App\Models\User;
+
+class StudentsCredentials extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'students:credentials';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import students for login credentials';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $students = Student::where('lrn','!=',0)->get();
+
+        $bar = $this->output->createProgressBar(count($students));
+
+        $this->info("Importing students credentials...");
+        $bar->start();
+
+        foreach ($students as $student) {
+
+            $check = User::where('student_id',$student->id)->first();
+
+            if (is_null($check)) {
+                $user = new User;
+                $user->lrn = $student->lrn;
+                $user->student_id = $student->id;
+
+                $lastname = strtoupper($student->lastname);
+                $dob = str_replace("-","",$student->date_of_birth);
+                $pw = $lastname.$dob;
+
+                $user->password = Hash::make($pw);
+                $user->save();
+            }
+
+            $bar->advance();
+
+        }
+
+        $bar->finish();
+
+        return 0;
+    }
+}
